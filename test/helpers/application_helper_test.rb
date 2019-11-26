ec7e0e0c233bc552470e9a5751e2ccffd1f63302
Fagnner <fagnnersousa@yahoo.com.br>
2019-11-26 01:30:57 -0300

    Add signup_path to home and write tests for the signup named route path and title

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -7,5 +7,6 @@ class ApplicationHelperTest < ActionView::TestCase
     assert_equal full_title("Help"), "Help | #{base_title}" , "This was not the correct title"
     assert_equal full_title("About"), "About | #{base_title}" , "This was not the correct title"
     assert_equal full_title("Contact"), "Contact | #{base_title}" , "This was not the correct title"
+    assert_equal full_title("Sign up"), "Sign up | #{base_title}" , "This was not the correct title"
   end
 end
