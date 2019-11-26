2e44608917018522fbcc02d82f3b4a68556f72e7
Fagnner <fagnnersousa@yahoo.com.br>
2019-11-26 00:56:19 -0300

    Create full title helper test

---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class ApplicationHelperTest < ActionView::TestCase  
+  base_title = "Ruby on Rails Tutorial Chuuiter App"
+  test "full title helper" do
+    assert_equal full_title,         base_title, "This was not the correct title"
+    assert_equal full_title("Help"), "Help | #{base_title}" , "This was not the correct title"
+    assert_equal full_title("About"), "About | #{base_title}" , "This was not the correct title"
+    assert_equal full_title("Contact"), "Contact | #{base_title}" , "This was not the correct title"
+  end
+end
